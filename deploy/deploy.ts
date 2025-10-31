8f20898a75c4c257ef93e62b2b8bc2fe6ce5442a
emma-jameser <tccjyfyz60680@outlook.com>
2025-10-31 16:35:39 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const DataWar = await deploy("DataWar", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`DataWar contract: `, DataWar.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_DataWar"; // id required to prevent reexecution
+func.tags = ["DataWar"];
